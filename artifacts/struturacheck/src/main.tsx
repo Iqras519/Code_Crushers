ebd441afaed66c024588e85936f7c9e1596b7990
Affan-codes-DS <Affan-codes-DS@users.noreply.github.com>
2026-06-10 16:20:00 +0530

    Revert "commit 2"
    This reverts commit b12669c2844ad280681d107683af7e610164be01.

--- artifacts/struturacheck/src/main.tsx
+++ artifacts/struturacheck/src/main.tsx
@@ -1,9 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import { configureApiClient } from "./lib/api-config";
 import "./index.css";
-
-// Configure API client before rendering app
-configureApiClient();
 
 createRoot(document.getElementById("root")!).render(<App />);
